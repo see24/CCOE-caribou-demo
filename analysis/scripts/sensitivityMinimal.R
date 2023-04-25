4df5b150a404bde97d263eea619907a7c35eba5e
see24 <sarahendicott4@gmail.com>
2023-04-25 15:29:48 -0400

    update sensitivityMinimal

--- analysis/scripts/sensitivityMinimal.R
+++ analysis/scripts/sensitivityMinimal.R
@@ -6,39 +6,20 @@
 #Rscript --vanilla ./analysis/scripts/sensitivityMinimal.R 1 "local"
 #nohup Rscript --vanilla ./analysis/scripts/sensitivityMinimal.R 1 "local" &
 
-args = commandArgs(trailingOnly=TRUE)
-#args=1
-if(args[2]!="local"){
-  baseDir = getwd()
-  workingDir = file.path(baseDir,"Caribou-Demographic-Projection-Paper")
-  toolDir = file.path(baseDir,"BayesianCaribouDemographicProjection")
-  libDir = NULL
-  print(workingDir)
-}else{
-  baseDir = "C:/Users/HughesJo/Documents"
-  workingDir = paste0(baseDir,"/gitprojects/Caribou-Demographic-Projection-Paper")
-  toolDir = paste0(baseDir,"/gitprojects/BayesianCaribouDemographicProjection")
-  libDir = paste0(baseDir,"/cloudDeploymentSandbox/Rpackages")
-}
+# Run batches from Rscript that uses parallel backend and new caribouMetrics functions
+library(doFuture)
+library(caribouMetrics)
 
-cpageId=args[1] #which batch?
 setName = "s3"
-message("batch ", args[1], " started")
+
 #######################
-setwd(workingDir)
 dir.create(paste0("figs/",setName),recursive=T)
 dir.create(paste0("tabs/",setName),recursive=T)
 dir.create(paste0("results/",setName),recursive=T)
 
-packages <- c("R2jags","gdata","mcmcplots",
-              "ggplot2","RODBC","plyr","dplyr","survival","gdata","data.table",
-              "tidyr","caribouMetrics")
-
-for(p in packages){library(p,lib.loc=libDir,character.only=T)}
+simBig<-getSimsNational() #If called with default parameters, use saved object to speed things up.
 
 allScns = read.csv(paste0("tabs/",setName,".csv"))
-scns = subset(allScns,pageId==cpageId)
-nrow(allScns)
 
 ####################
 eParsIn = list()
@@ -51,15 +32,19 @@ eParsIn$collarOnTime=1
 eParsIn$collarOffTime=12
 eParsIn$collarNumYears=6
 
+future::plan("multisession")
 
-setwd(toolDir)
+foreach(cpageId = 1:n_batches, .options.future = list(seed = TRUE)) %dofuture% {
 
-source("CaribouDemoFns.R")
-simBig<-getSimsNational() #If called with default parameters, use saved object to speed things up.
+  scns = subset(allScns, pageId==cpageId)
 
-scResults = runScnSet(scns,eParsIn,simBig,getKSDists=F,printProgress=F)
-setwd(workingDir)
+  message("batch ", cpageId, " started")
 
-saveRDS(scResults,paste0("results/",setName,"/r",cpageId,".Rds"))
+  scResults = caribouMetrics:::runScnSet(scns,eParsIn,simBig,getKSDists=F,printProgress=F)
 
-message("batch ", args[1], " completed")
+  saveRDS(scResults,paste0("results/",setName,"/r",cpageId,".Rds"))
+
+  message("batch ", cpageId, " complete")
+
+}
+future::plan("sequential")
--- analysis/scripts/sensitivityMinimalTestPar.R
+++ analysis/scripts/sensitivityMinimalTestPar.R
@@ -1,55 +1,48 @@
 #!/usr/bin/env Rscript
 
+
 # Run batches from Rscript that uses parallel backend and new caribouMetrics functions
-n_batches <- 6
+n_batches <- 1
 n_scns <- 2
 
-setName = "s2"
+library(doFuture)
+library(caribouMetrics)
+
+setName = "s3"
 
 #######################
 dir.create(paste0("figs/",setName),recursive=T)
 dir.create(paste0("tabs/",setName),recursive=T)
 dir.create(paste0("results/",setName),recursive=T)
 
-packages <- c("R2jags","gdata","mcmcplots",
-              "ggplot2","RODBC","plyr","dplyr","survival","gdata","data.table",
-              "tidyr","caribouMetrics")
-
-for(p in packages){library(p,character.only=T)}
-
 simBig<-getSimsNational() #If called with default parameters, use saved object to speed things up.
-
-
 
 allScns = read.csv(paste0("tabs/",setName,".csv"))
 
-library(doFuture)
+####################
+eParsIn = list()
+eParsIn$cowCounts <- data.frame(Year = 1981:2023,
+                                Count = 100,
+                                Class = "cow")
+eParsIn$freqStartsByYear <- data.frame(Year = 1981:2023,
+                                       numStarts = 30)
+eParsIn$collarOnTime=1
+eParsIn$collarOffTime=12
+eParsIn$collarNumYears=3
 
 future::plan("multisession")
 
-foreach(cpageId = 1:n_batches) %dofuture% {
+foreach(cpageId = 1:n_batches, .options.future = list(seed = TRUE)) %dofuture% {
 
-  scns = subset(allScns,pageId==cpageId)
-  nrow(allScns)
+  scns = subset(allScns, pageId==cpageId)
 
   message("batch ", cpageId, " started")
 
-  ####################
-  eParsIn = list()
-  eParsIn$cowCounts <- data.frame(Year = 1981:2023,
-                                  Count = 100,
-                                  Class = "cow")
-  eParsIn$freqStartsByYear <- data.frame(Year = 1981:2023,
-                                         numStarts = 30)
-  eParsIn$collarOnTime=1
-  eParsIn$collarOffTime=12
-  eParsIn$collarNumYears=3
-
   scResults = caribouMetrics:::runScnSet(scns[1:n_scns,],eParsIn,simBig,getKSDists=F,printProgress=F)
-
 
   saveRDS(scResults,paste0("results/",setName,"/rTest",cpageId,".Rds"))
 
   message("batch ", cpageId, " complete")
 
 }
+future::plan("sequential")
