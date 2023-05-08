2a31cb04ab103a9dc7b7132787660b3dbe18469f
see24 <sarahendicott4@gmail.com>
2023-05-08 11:44:58 -0400

    do same for main script

--- analysis/scripts/sensitivityMinimal.R
+++ analysis/scripts/sensitivityMinimal.R
@@ -17,27 +17,31 @@ dir.create(paste0("figs/",setName),recursive=T)
 dir.create(paste0("tabs/",setName),recursive=T)
 dir.create(paste0("results/",setName),recursive=T)
 
-simBig<-getSimsNational() #If called with default parameters, use saved object to speed things up.
 
-allScns = read.csv(paste0("tabs/",setName,".csv"))
-n_batches <- length(unique(allScns$pageId))
-
-####################
-eParsIn = list()
-eParsIn$cowCounts <- data.frame(Year = 1981:2023,
-                                Count = 100,
-                                Class = "cow")
-eParsIn$freqStartsByYear <- data.frame(Year = 1981:2023,
-                                       numStarts = 30)
-eParsIn$collarOnTime=1
-eParsIn$collarOffTime=12
-eParsIn$collarNumYears=6
 
 future::plan("multisession")
 
 foreach(cpageId = 1:n_batches, .options.future = list(seed = TRUE)) %dofuture% {
 
+  simBig<-getSimsNational() #If called with default parameters, use saved object to speed things up.
+
+  allScns = read.csv(paste0("tabs/",setName,".csv"))
+  n_batches <- length(unique(allScns$pageId))
+
+  ####################
+  eParsIn = list()
+  eParsIn$cowCounts <- data.frame(Year = 1981:2023,
+                                  Count = 100,
+                                  Class = "cow")
+  eParsIn$freqStartsByYear <- data.frame(Year = 1981:2023,
+                                         numStarts = 30)
+  eParsIn$collarOnTime=1
+  eParsIn$collarOffTime=12
+  eParsIn$collarNumYears=6
+
   scns = subset(allScns, pageId==cpageId)
+
+  rm(allScns)
 
   message("batch ", cpageId, " started")
 
